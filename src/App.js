88a8b05b79c739e4b570134ae8147dd59aa3dafd
Banda-Aakash <aakashbanda03@gmail.com>
2024-03-24 20:40:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
+import Register from './components/Register';
+import VideoUpload from './components/Uploadvideo';
+import VideoPlayer from './components/Videoplayer';
+import Facialvarification from './components/Facialvarification';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+          <Route exact path="/fv" element={<Facialvarification/>}></Route>
+          <Route exact path="/" element={<Login/>}></Route>
+          <Route exact path="/register" element={<Register/>}></Route>
+          <Route exact path="/upload" element={<VideoUpload/>}></Route>
+          <Route exact path="/result" element={<VideoPlayer/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
